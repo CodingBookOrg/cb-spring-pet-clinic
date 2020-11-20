dea44a53fe89f7381197e68ce185dd627e960e7f
orhan <orhanu2020@gmail.com>
2020-11-19 20:34:01 -0500

    added initial project files

---
+++ src/test/java/org/cb/cbspringpetclinic/CbSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cb.cbspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
