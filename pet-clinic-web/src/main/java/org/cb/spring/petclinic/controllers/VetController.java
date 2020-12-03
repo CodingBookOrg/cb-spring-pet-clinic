13641cf696fb778f79d65180e243a81e4aace4f1
orhan <orhanu2020@gmail.com>
2020-12-03 18:40:11 -0500

    refactorings

--- pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/VetController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({"vets"})
-public class VetsController {
+@RequestMapping({"vet"})
+public class VetController {
 
     @RequestMapping({ "/", "/index.html"})
     public String list() {
-        return "vets/index";
+        return "vet/index";
     }
 }
