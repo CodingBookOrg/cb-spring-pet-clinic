c9ca0fcce85d6acd36fc97f1a88fe6fcea8a4f0f
orhan <orhanu2020@gmail.com>
2020-12-03 15:57:46 -0500

    added unversioned files

---
+++ pet-clinic-web/src/main/java/org/cb/spring/petclinic/PetClinicWebApplication.java
@@ -0,0 +1,13 @@
+package org.cb.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWebApplication.class, args);
+    }
+
+}
---
+++ pet-clinic-web/src/test/java/org/cb/spring/petclinic/PetClinicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cb.spring.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
