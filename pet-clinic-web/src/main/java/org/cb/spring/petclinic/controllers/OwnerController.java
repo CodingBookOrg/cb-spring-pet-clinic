cea2a8672c573d2319997be57d3fceffabf683fb
orhan <orhanu2020@gmail.com>
2020-12-14 22:20:26 -0500

    List owners

--- pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package org.cb.spring.petclinic.controllers;
 
+import org.cb.spring.petclinic.services.IOwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"owner"})
 public class OwnerController {
+
+    private final IOwnerService ownerService;
+
+    public OwnerController(IOwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", "index.html"})
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
