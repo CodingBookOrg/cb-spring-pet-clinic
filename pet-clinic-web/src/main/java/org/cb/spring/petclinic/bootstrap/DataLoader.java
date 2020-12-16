90406f4737a1a18f1db6385b87a3f9e9a62c1afa
orhan <orhanu2020@gmail.com>
2020-12-15 20:06:41 -0500

    List vets

--- pet-clinic-web/src/main/java/org/cb/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/cb/spring/petclinic/bootstrap/DataLoader.java
@@ -20,29 +20,29 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1l);
+        owner1.setId(3l);
         owner1.setFirstName("O1");
-        owner1.setFirstName("OO1");
+        owner1.setLastName("OO1");
         ownerService.save(owner1.getId(), owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2l);
+        owner2.setId(4l);
         owner2.setFirstName("O2");
-        owner2.setFirstName("OO2");
+        owner2.setLastName("OO2");
         ownerService.save(owner2.getId(), owner2);
 
         System.out.println("Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1l);
+        vet1.setId(3l);
         vet1.setFirstName("V1");
-        vet1.setFirstName("VV1");
+        vet1.setLastName("VV1");
         vetService.save(vet1.getId(), vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1l);
+        vet2.setId(4l);
         vet2.setFirstName("V2");
-        vet2.setFirstName("VV2");
+        vet2.setLastName("VV2");
         vetService.save(vet2.getId(), vet2);
 
         System.out.println("Loaded vets");
--- pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/cb/spring/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package org.cb.spring.petclinic.controllers;
 
+import org.cb.spring.petclinic.services.IVetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"vet"})
 public class VetController {
 
+    private final IVetService vetService;
+
+    public VetController(IVetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({ "/", "/index.html"})
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
